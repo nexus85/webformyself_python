7c488d8330758af9b7602095519129ee45575b0f
nexus <mx38@mail.ru>
2020-01-06 14:06:07 +0300

    01/06/20

---
+++ spec/spec.py
@@ -0,0 +1,104 @@
+# name = input('введите ваше имя: ')
+# age = input('Сколько вам лет: ')
+#
+#
+#
+#
+# greet = 'Привет, ' + name
+#
+# greet = greet + (f' Тебе {age} лет')
+# print(greet)
+#
+
+#
+# w = int(input('Введите сторону прямоугольника: '))
+# h = int(input('Введите другую сторону прямоугольника: '))
+#
+#
+# # s = f'{w}'
+# s = f'Площать прямогуольника {w}x{h} = {h*w}'
+# print(s)
+#
+# name = input('Введите ваше имя: ')
+# age = input('Сколько вам лет?: ')
+#
+#
+#
+# greet = f'Привет, {name}.\n\tТебя {age} лет.'
+# print(greet)
+
+
+# print(r'Ру'
+#       r''
+#       r'ываы'
+#       r''
+#       r'выаы'
+#       r'ываыаываыв'
+#       r'выавыаыываываываы c:\sdfsdfs\sdf') #raw строка
+
+# s1 = 'hello'
+# s2 = 'world'
+# print(s1, s2, sep='!', end='\t\t')
+# print(s2)
+#
+# ))import random
+# #
+# # # n1 = random.randrange(5)
+# # n1 = random.randrange(1, 11) # от 1 до 10 включительно
+# # n2 = random.randrange(1, 11)
+# #
+# #
+# #
+# #
+# # answer = input(f'Сколько будет {n1}+{n2}: ' )
+# # test = answer.replace('.', '', 1) # ищем строчку, одну и меняемя ее
+# # if not test.isdigit():
+# #     print('Надо ввести число!')
+# # else:
+# #     if answer == test:
+# #         answer = int(answer)
+# #     else:
+# #         answer = float(answer)
+# #     if int(answer) == n1+n2:
+# #         print('Вы правы')
+# #     else:
+# #         print(f'Вы не правы, правильный ответ {n1+n2}, а вы ответили {answer}')
+# #
+# #
+# # print('12.342'.replace('.',''))
+# # print('23.23.23'.replace('.','',1
+
+#
+#
+# n1 = 2
+# n2 = 3
+#
+# if n1 > n2:
+#     print('Больше')
+# elif n1<n2:
+#     print('Меньше')
+# else:
+#     print('Равно')
+
+
+import random
+
+num = random.randrange(1, 11)
+
+while True:
+    # test = answer.replace('.','',1)
+    answer = input('введите число: ')
+    if not answer.isdigit():
+            # print(answer)
+        print('Надо ввесит число')
+    else:
+        answer = int(answer)
+        if answer > num:
+            print('Меньше')
+        elif answer < num:
+            print('Больше')
+        else:
+            print('Угадали')
+            break
+
+

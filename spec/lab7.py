00f0e06d3c2e6c0fa5e63c457400a36fd8d94e71
nexus <mx38@mail.ru>
2020-01-07 20:04:35 +0300

    модуль 7 - практикум 4

--- spec/lab7.py
+++ spec/lab7.py
@@ -1,10 +1,34 @@
 
 
-def read_file():
-    pass
-def save_file():
-    pass
+def read_file(file):
 
+    with open(file, 'r', encoding='utf-8') as f:
+        count = f.readline()
+        list1 = []
+        for i in f.readlines():
+            list1.append(i.strip())
+        # lines = f.readlines()
+        # print(f)
+        # print(count)
+        # for i in range(int(count)):
+        #     if i == 0:
+        #         continue
+        #     else:
+        #         list1.append(f.readline, i)
+        # print(lines)
+    return count, list1
+
+# def save_file():
+#     pass
+try:
+    x, y = read_file('lab7')
+except Exception as e:
+    print(e)
+except:
+    print('Ошибка какая то')
+else:
+    print(x)
+    print(y)
 
 
 
